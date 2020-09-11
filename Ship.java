7a089d8bff1dc6899368dfd4720d2da06e95bd66
Jessica <jbjb2200@gmail.com>
2020-09-10 20:17:48 -0700

    Added comments

--- Ship.java
+++ Ship.java
@@ -12,15 +12,18 @@ public class Ship {
     private int length;
     private String title;
     
+    // creates a ship with a title and length
     public Ship (String title, int length) {
         this.title = title;
         this.length = length;
     }
     
+    // returns an int value representing the amount of spaces the ship occupies
     public int getLength() {
         return length;
     }
     
+    // returns a String representing the ship's name
     public String getTitle() {
         return title;
     }
